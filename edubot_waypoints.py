b4199407e2ee0ca361a774b3837e551769ffb42c
iralukas <irinalukashova03@mail.ru>
2023-04-13 15:40:06 +0300

    поток для обновления телеметрии

--- edubot_waypoints.py
+++ edubot_waypoints.py
@@ -6,6 +6,7 @@ from gs_lps import *
 import sys
 import math
 import time
+import threading
 
 KR = 35
 KD = 4
@@ -17,22 +18,26 @@ dist_max = 1
 
 class WaypointsRobot:
     def __init__(self, logger=True):
-
         self.edubot = edubot2.EduBot(enableDisplay=False)
         self.edubot.Start()
 
-        self._nav = us_nav()
-        self._nav.start()
-
         self._time_start = time.time()
         self._battery_watch_timer = time.time()
-
-        self._telemetery_timer = 0.05
         self._logger = logger
+
+        self._telemetery_thread = threading.Thread(target=self._update_coordinates, daemon=True)
+        self._telemetery_thread.daemon = True
+        self._telemetery_thread.start()
+        self._telemetery_timeout = 0.025
+        self._telemetery_send_time = time.time() - self._telemetery_timeout
 
         self._x_filter = MedianFilter()
         self._y_filter = MedianFilter()
         self._yaw_filter = MedianFilter()
+
+        self.x = 0
+        self.y = 0
+        self.yaw = 0
 
     def _data_write(self, *values):  # конкатенирует все аргументы в строку и записывает в файл
         if self._logger:
@@ -47,20 +52,20 @@ class WaypointsRobot:
         return False
 
     def go_to_local_point_body_fixed(self, dx, dy, driving):  # доехать до точки с заданным смещением относительно текущей позиции
-        x, y, yaw = self._update_coordinates()
+        x, y, yaw = self.x, self.y, self.yaw
         return self.go_to_local_point(x + dx, y + dy, driving)
 
     def _rotate_to_target(self, x_target, y_target, is_driving):  # повернуться в сторону заданной точки
         e_prev = 0
         e_sum = 0
-        x, y, yaw = self._update_coordinates()
+        x, y, yaw = self.x, self.y, self.yaw
         angle = normalize_angle(math.atan2(y_target - y, x_target - x) - yaw)
         while abs(angle) > 0.03:
             if not is_driving.is_set():
                 self.stop()
                 return False
             self._check_battery()
-            x, y, yaw = self._update_coordinates()
+            x, y, yaw = self.x, self.y, self.yaw
             angle = normalize_angle(math.atan2(y_target - y, x_target - x) - yaw)
             e_sum += angle
             u_s = 0
@@ -74,7 +79,7 @@ class WaypointsRobot:
 
     def _straight_to_target(self, x_target, y_target, driving):  # движение в точку с динамической коррекцией угла
         e_prev = 0
-        x, y, yaw = self._update_coordinates()
+        x, y, yaw = self.x, self.y, self.yaw
         x_start, y_start = x, y
         dist = math.sqrt((x_target - x) ** 2 + (y_target - y) ** 2)
         while dist > LIMIT:
@@ -82,7 +87,7 @@ class WaypointsRobot:
                 self.stop()
                 return False
             self._check_battery()
-            x, y, yaw = self._update_coordinates()
+            x, y, yaw = self.x, self.y, self.yaw
             dist = math.sqrt((x_target - x) ** 2 + (y_target - y) ** 2)
             dist_start = math.sqrt((x_start - x) ** 2 + (y_start - y) ** 2)
             angle = normalize_angle(math.atan2(y_target - y, x_target - x) - yaw)
@@ -107,25 +112,24 @@ class WaypointsRobot:
         self.edubot.leftMotor.SetParrot(round(-left))
 
     def _update_coordinates(self):  # получение координат и угла рыскания с локуса
+        self._nav = us_nav()
+        self._nav.start()
         while True:
             pos = self._nav.get_position()
             angles = self._nav.get_angles()
             if pos is not None and angles is not None:  # при сбое могут прийти координаты (0, 0)
                 x, y = self._x_filter.filter(pos[0]), self._y_filter.filter(pos[1])
                 yaw = self._yaw_filter.filter(angles[2]) + 3.14  # поворот системы координат, чтобы угол отсчитывался от оси x
-                return x, y, yaw  # фильтрация на предмет выбросов
+                if time.time() - self._telemetery_send_time >= self._telemetery_timeout:
+                    self._telemetery_send_time = time.time()
+                    self.x, self.y, self.yaw = x, y, yaw
+
 
     def get_local_position(self):
-        while True:
-            pos = self._nav.get_position()
-            if pos is not None:
-                return pos #x, y, z
+        return [self.x, self.y, 0]
 
     def get_attitude(self):
-        while True:
-            angles = self._nav.get_angles()
-            if angles is not None:
-                return angles
+        return [0, 0, self.yaw]
 
     def _check_battery(self):
         if time.time() - self._battery_watch_timer >= 5:  # проверка заряда каждые 5 секунд
@@ -152,6 +156,7 @@ class WaypointsRobot:
 
 class MedianFilter:  # медианный фильтр для фильтрации одиночных выбросов
     history = [-10]
+
     def filter(self, x):
         self.history.append(x)
         while len(self.history) > 3:
